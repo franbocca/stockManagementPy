d66246ff565cd4cf030e59fdd85c218895b0c8d4
Sisco <fboccalatte@gmail.com>
2019-05-04 01:33:19 -0300

    Primeros archivos

---
+++ model.py
@@ -0,0 +1,10 @@
+class Proveedor:
+    cuit = None
+    razonSocial = None
+    nombreAlias = None
+    direccionPostal = None
+    descripcion = None
+    tipoDePago = None
+    tipoDeCambio = None
+    retencionIIBB = None
+    

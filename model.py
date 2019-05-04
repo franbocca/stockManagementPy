84eb370a787b7a9960b3b1f14b62c5cb8fd9f92c
Sisco <fboccalatte@gmail.com>
2019-05-04 01:59:13 -0300

    Comenzando a modelar estructuras

--- model.py
+++ model.py
@@ -1,7 +1,12 @@
-class Proveedor:
-    cuit = None
-    razonSocial = None
-    nombreAlias = None
+class Persona:
+    tipo = None
+    tipo_documento = None
+    nro_documento = None
+    dig_documento = None
+    id_fiscal = None
+    condicion_fiscal = None
+    nombre = None
+    alias = None
     direccionPostal = None
     descripcion = None
     tipoDePago = None
